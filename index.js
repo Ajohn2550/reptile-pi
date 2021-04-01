afd1a779d4287ddb1ef0bbfde9efd1b5d52fbd08
Alex Johnson <alex@johnsonwebware.com>
2021-03-31 22:56:09 -0700

    maths

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const sensor = require('node-dht-sensor');
 
 sensor.read(11, 4, (err, temperature, humidity) => {
     if(!err) {
-        let ftemp = temperature * (5/9) + 32;
+        let ftemp = (temperature * (5/9)) + 32;
         console.log(`Temp: ${ftemp} C Dumidity: ${humidity}%`);
     } else {
         console.err(err);
