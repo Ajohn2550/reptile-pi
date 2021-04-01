5556b1277ddecb499da128038235eec869b92f8d
Alex Johnson <alex@johnsonwebware.com>
2021-04-01 00:29:21 -0700

    Day/Night logic

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const { getSunrise, getSunset } = require('sunrise-sunset-js');
+const moment = require('moment');
 const sensor = require('node-dht-sensor').promises;
+
+const lat = '33.448376';
+const lon = '-112.074036';
+const dayTemp = 100;
+const nightTemp = 75;
+
+const sunrise = getSunrise(lat, lon);
+const sunset = getSunset(lat, lon);
 
 sensor.setMaxRetries(5);
 sensor.initialize(11, 4);
@@ -6,7 +16,6 @@ sensor.initialize(11, 4);
 const ctof = c => c * 9/5 + 32;
 
 function getTemp() {
-    setTimeout(getTemp, 5000);
     sensor.read(11, 4)
     .then((res) => {
         console.log(`Current Temperature: ${ctof(res.temperature)} F Humidity: ${res.humidity.toFixed(1)}%`);
@@ -14,4 +23,17 @@ function getTemp() {
     .catch(console.error);
 }
 
-getTemp();
+function loop() {
+    setInterval(loop, 5000);
+    let isDatytime = moment().isBetween(sunrise, sunset);
+
+    if(isDatytime) {
+        console.log(`Daytime - Temp set to: ${dayTemp}`);
+    } else {
+        console.log(`Nighttime - Temp set to: ${nightTemp}`);
+    }
+    getTemp();
+
+}
+
+loop();
