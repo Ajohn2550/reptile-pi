92da5dbb6d7226f098922e7fd079dfb4d32d1ed2
Alex Johnson <alex@johnsonwebware.com>
2021-03-31 23:15:17 -0700

    loop every 5 seconds

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const sensor = require('node-dht-sensor');
+const sensor = require('node-dht-sensor').promises;
 
-sensor.read(11, 4, (err, temperature, humidity) => {
-    if(!err) {
-        let ftemp = temperature * (9/5) + 32;
-        console.log(`Temp: ${ftemp} C Dumidity: ${humidity}%`);
-    } else {
-        console.err(err);
-    }
-});
+sensor.setMaxRetries(5);
+sensor.initialize(11, 4);
 
+const ctof = c => c * 9/5 + 32;
+
+function getTemp() {
+    sensor.read(11, 4)
+    .then((res) => {
+        console.log(`Current Temperature: ${ctof(res.temperature)} F Humidity: ${res.humidity.toFixed(1)}%`);
+    })
+    .catch(console.error)
+    then(() => {
+        setTimeout(foo, 5000);
+    });
+}
+
+getTemp();
