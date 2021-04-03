9854255f97999e3b00d72dc1bdd8f4b7539cc856
Alex Johnson <alex@johnsonwebware.com>
2021-04-03 16:47:30 -0700

    Making Modules

--- index.js
+++ index.js
@@ -1,45 +1,29 @@
 const SunCalc = require('suncalc-tz');
 const moment = require('moment');
 const cron = require('node-cron');
-const sensor = require('node-dht-sensor').promises;
 const config = require('config');
+const AmbientSensor = require('./lib/sensors/ambient')
 
+const tankAmbient = new AmbientSensor(11, config.get('pins.ambient'));
 let sunrise;
 let sunset;
+let ttemp = 75;
 
 function updateDaylight() {
-    let suntime = SunCalc.getTimes(
-        Date.now(),
-        config.get('location.latitude'),
-        config.get('location.longitude')
-    );
+    let suntime = SunCalc.getTimes(Date.now(), config.get('location.latitude'), config.get('location.longitude'));
     sunrise = suntime.sunrise;
     sunset = suntime.sunset;
     console.log(`Updating Daylight. Sunrise: ${sunrise} Sunset: ${sunset}`);
 }
 
-sensor.setMaxRetries(5);
-sensor.initialize(11, 4);
-
-const ctof = c => c * 9/5 + 32;
-
-function getTemp() {
-    sensor.read(11, 4)
-    .then((res) => {
-        console.log(`Current Temperature: ${ctof(res.temperature)} F Humidity: ${res.humidity.toFixed(1)}%`);
-    })
-    .catch(console.error);
-}
+const isDayLight = () => moment().isBetween(sunrise, sunset);
 
 function loop() {
-    let isDatytime = moment().isBetween(sunrise, sunset);
-
-    if(isDatytime) {
-        console.log(`Daytime - Temp set to: ${config.get('temperatures.day')}`);
+    if(isDayLight) {
+        console.log(`Daytime - Temp set to: ${config.get('temperatures.day')} Current Temp: ${tankAmbient.currentTemperature()}`);
     } else {
-        console.log(`Nighttime - Temp set to: ${config.get('temperatures.night')}`);
+        console.log(`Nighttime - Temp set to: ${config.get('temperatures.night')} Current Temp: ${tankAmbient.currentTemperature()}`);
     }
-    getTemp();
 }
 
 updateDaylight();
