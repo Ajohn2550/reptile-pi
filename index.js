0c1f6e855c6666878c6ec0a77ec50b6b5e4142a0
Alex Johnson <alex@johnsonwebware.com>
2021-08-06 20:26:37 -0700

    updating sun time library

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-const SunCalc = require('suncalc-tz');
 const moment = require('moment');
 const cron = require('node-cron');
 const config = require('config');
+
+const SunTime = require('./lib/time/suntime');
 const AmbientSensor = require('./lib/sensors/ambient')
 
+const sunTime = new SunTime(config.get('location.latitude'), config.get('location.longitude'));
 const tankAmbient = new AmbientSensor(11, config.get('pins.ambient'));
-let sunrise;
-let sunset;
-let ttemp = 75;
 
-function updateDaylight() {
-    let suntime = SunCalc.getTimes(Date.now(), config.get('location.latitude'), config.get('location.longitude'));
-    sunrise = suntime.sunrise;
-    sunset = suntime.sunset;
-    console.log(`Updating Daylight. Sunrise: ${sunrise} Sunset: ${sunset}`);
-}
-
-const isDayLight = () => moment().isBetween(sunrise, sunset);
 
 function loop() {
-    if(isDayLight) {
+    if(suntime.isDayLight) {
         console.log(`Daytime - Temp set to: ${config.get('temperatures.day')} Current Temp: ${tankAmbient.currentTemperature()}`);
     } else {
         console.log(`Nighttime - Temp set to: ${config.get('temperatures.night')} Current Temp: ${tankAmbient.currentTemperature()}`);
     }
 }
 
-updateDaylight();
-cron.schedule('* * * * *', updateDaylight);
 cron.schedule('0,15,30,45 * * * * *', loop);
